2110760c20a8545ac29f913e1897718185c2016f
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-08-08 16:49:17 +0400

    flatmap

--- app/src/main/java/com/codingwithrufat/rxjava/MainActivity.java
+++ app/src/main/java/com/codingwithrufat/rxjava/MainActivity.java
@@ -1,23 +1,29 @@
 package com.codingwithrufat.rxjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.codingwithrufat.rxjava.model.Task;
-import com.codingwithrufat.rxjava.repository.DataSource;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codingwithrufat.rxjava.model.Comments;
+import com.codingwithrufat.rxjava.model.Posts;
+import com.codingwithrufat.rxjava.network.ApiClient;
+import com.codingwithrufat.rxjava.network.IApi;
+
+import java.util.List;
+import java.util.Random;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Consumer;
-import io.reactivex.rxjava3.functions.Predicate;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,63 +31,60 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     // ui
-    private TextView text;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     // variables
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private IApi iApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iApi = ApiClient.getRetrofit().create(IApi.class);
 
-        Observable<Task> taskObservable = Observable
-                .fromIterable(DataSource.getTasksList())
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerViewAdapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        getData();
+
+    }
+
+    private void getData() {
+
+        getObservablePosts()
                 .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() { // background proses
+                .flatMap(new Function<Posts, ObservableSource<Posts>>() {
                     @Override
-                    public boolean test(Task task) throws Throwable {
-                        Log.d(TAG, "test: " + Thread.currentThread().getName());
-                        return task.isComplete();
+                    public ObservableSource<Posts> apply(Posts posts) throws Throwable {
+                        return getObservableComments(posts);
                     }
-                })
-                .observeOn(AndroidSchedulers.mainThread());
+                }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Posts>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        compositeDisposable.add(d);
+                    }
 
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "onSubscribe: called");
+                    @Override
+                    public void onNext(@NonNull Posts posts) {
+                        recyclerViewAdapter.updatePost(posts);
+                    }
 
-                compositeDisposable.add(d);
+                    @Override
+                    public void onError(@NonNull Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onNext(@NonNull Task task) {
-                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
+                    @Override
+                    public void onComplete() {
 
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.d(TAG, "onError: " + e.getMessage());
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called");
-
-            }
-        });
-
-        compositeDisposable.add(taskObservable.subscribe(new Consumer<Task>() {
-            @Override
-            public void accept(Task task) throws Throwable {
-
-            }
-        }));
+                    }
+                });
 
     }
 
@@ -91,4 +94,40 @@ public class MainActivity extends AppCompatActivity {
         compositeDisposable.clear();
         compositeDisposable.dispose();
     }
+
+    private Observable<Posts> getObservablePosts() {
+
+        return iApi.getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<List<Posts>, ObservableSource<Posts>>() {
+                    @Override
+                    public ObservableSource<Posts> apply(List<Posts> posts) throws Throwable {
+                        Log.d(TAG, "apply: " + posts.get(0).getTitle());
+                        recyclerViewAdapter.setPosts(posts);
+                        return Observable.fromIterable(posts)
+                                .subscribeOn(Schedulers.io());
+                    }
+                });
+
+    }
+
+    private Observable<Posts> getObservableComments(Posts posts) {
+
+        return iApi.getComments(posts.getId())
+                .map(new Function<List<Comments>, Posts>() {
+                    @Override
+                    public Posts apply(List<Comments> comments) throws Throwable {
+
+                        int delay = ((new Random()).nextInt(5) + 1) * 1000; // sleep thread for x ms
+                        Thread.sleep(delay);
+
+                        posts.setComments(comments);
+                        return posts;
+                    }
+                })
+                .observeOn(Schedulers.io());
+
+    }
+
 }
---
+++ app/src/main/java/com/codingwithrufat/rxjava/network/ApiClient.java
@@ -0,0 +1,26 @@
+package com.codingwithrufat.rxjava.network;
+
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+
+    public static Retrofit getRetrofit(){
+
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+
+    }
+
+}
