f93d49d00df63be571420878c375af92b2234a41
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-08-09 11:38:43 +0400

    throttleFirst

--- app/src/main/java/com/codingwithrufat/rxjava/MainActivity.java
+++ app/src/main/java/com/codingwithrufat/rxjava/MainActivity.java
@@ -2,12 +2,18 @@ package com.codingwithrufat.rxjava;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 
+import com.jakewharton.rxbinding4.view.RxView;
+
+import java.util.Currency;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
@@ -16,6 +22,7 @@ import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private long timeSinceLastRequest;
 
     // ui
-    private SearchView searchView;
+    private Button button;
 
 
     @Override
@@ -34,66 +41,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchView = findViewById(R.id.search_view);
+        button = findViewById(R.id.button);
+
 
         timeSinceLastRequest = System.currentTimeMillis();
 
-        Observable<String> stringObservable = Observable
-                .create(new ObservableOnSubscribe<String>() {
+        /**
+         * clicked button and you clicked times it is not working
+         * throttleFirst is for after 4000 ms it will be active
+          */
+        RxView.clicks(button)
+                .throttleFirst(4000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Unit>() {
                     @Override
-                    public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+                    public void onSubscribe(@NonNull Disposable d) {
+                        compositeDisposable.add(d);
+                    }
 
-                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                            @Override
-                            public boolean onQueryTextSubmit(String query) {
-                                return false;
-                            }
+                    @Override
+                    public void onNext(@NonNull Unit unit) {
+                        Log.d(TAG, "onNext: time since last clicked" + (System.currentTimeMillis() - timeSinceLastRequest));
+                        timeSinceLastRequest = System.currentTimeMillis();
+                        doSomething();
+                    }
 
-                            @Override
-                            public boolean onQueryTextChange(String newText) {
-                                if (!emitter.isDisposed()){ // pass the query to the emitter
-                                    emitter.onNext(newText);
-                                }
-                                return false;
-                            }
-                        });
+                    @Override
+                    public void onError(@NonNull Throwable e) {
 
                     }
-                })
-                .debounce(1000, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io());
 
-        stringObservable.subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                compositeDisposable.add(d);
-            }
+                    @Override
+                    public void onComplete() {
 
-            @Override
-            public void onNext(@NonNull String s) {
-                Log.d(TAG, "onNext: time since last request: " + (System.currentTimeMillis() - timeSinceLastRequest));
-                Log.d(TAG, "onNext: query is " + s);
-                timeSinceLastRequest = System.currentTimeMillis();
-                sendRequestToApi(s);
-            }
+                    }
+                });
 
-            @Override
-            public void onError(@NonNull Throwable e) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
 
     }
 
-    private void sendRequestToApi(String query){
-        // do something
+    private void doSomething() {
+        Toast.makeText(this, "execute", Toast.LENGTH_SHORT).show();
     }
-
 
 
     @Override
